6c3a0977fe7d2f2fede9f796efcd3fe0d52a7a9b
Gravy Jones <122278384+GravyJonesGalleon@users.noreply.github.com>
2024-11-08 12:21:19 +0000

    Owen valid buildings (#4)
    * added checks for building placement validity
    
    * made buildings tell the map when they occupy a cell

--- core/src/main/java/io/github/unisim/Accommodation.java
+++ core/src/main/java/io/github/unisim/Accommodation.java
@@ -1,10 +1,11 @@
 package io.github.unisim;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Accommodation extends Building {
-    public Accommodation(FitViewport viewport) {
-        super(new Texture("accommodation.png"), viewport);
+    public Accommodation(FitViewport viewport, TiledMap tiledMap) {
+        super(new Texture("accommodation.png"), viewport, tiledMap);
     }
 }
